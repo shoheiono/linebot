07448f390c7119bcb93f86ed68cf7df25baf5caf
shohei ono <s.ono@gloops.com>
2016-04-20 16:33:07 +0900

    add post callback

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var request = require('request');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -9,13 +10,55 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
+app.get('/', function(req, response) {
 	response.render('pages/index');
 });
 
-app.post('/callback', function(request, response) {
-	console.log(request.body);
-	response.send('Hello World!');
+app.post('/callback', function(req, res) {
+	var json = req.body;
+	console.log(json);
+
+	res.send(req);
+	//ヘッダーを定義
+	var headers = {
+		'Content-Type' : 'application/json; charset=UTF-8',
+		'X-Line-ChannelID' : '<Your Channel ID>',
+		'X-Line-ChannelSecret' : '<Your Channel Secret>',
+		'X-Line-Trusted-User-With-ACL' : '<Your MID>'
+	};
+	// 送信相手の設定（配列）
+	var to_array = [];
+	to_array.push(json['result'][0]['content']['from']);
+	// 送信データ作成
+	var data = {
+		'to': to_array,
+		'toChannel': 1383378250,			//固定
+		'eventType':'140177271400161403',	//固定
+		"content": {
+			"messageNotified": 0,
+			"messages": [
+				{
+					"contentType": 1,
+					"text": 'こんにちは',
+				}
+			]
+		}
+	};
+	//オプションを定義
+	var options = {
+		url: 'https://trialbot-api.line.me/v1/events',
+		proxy : process.env.FIXIE_URL,
+		headers: headers,
+		json: true,
+		body: data
+	};
+	req.post(options, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			console.log(body);
+		} else {
+			console.log('error: '+ JSON.stringify(response));
+		}
+	});
 });
 
 app.listen(app.get('port'), function() {
